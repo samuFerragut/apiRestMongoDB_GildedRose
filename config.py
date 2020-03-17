e032dac56ea00506987c53da71b55b33988797f4
javiBarrientos <javibarrientosfernandez99@gmail.com>
2020-03-17 14:09:47 +0100

    configurar repository db_atlas

--- config.py
+++ config.py
@@ -2,3 +2,4 @@
 
 
 # tienda = SingletonOllivander.crearTienda()
+SECRET_KEY = b"R\x8f\x0eK\xbfc\x93y\xae\xb3e\x16\x9cd\xb2'"
--- repository/db_atlas.py
+++ repository/db_atlas.py
@@ -21,7 +21,7 @@ def get_db():
         # Conectarse a Mongo Atlas con mongoengine
         # En la URI, /test? es la bbdd
         g.db = connect(
-                    host='URI'
+                    host='mongodb+srv://javiBarrientos:pass@cluster-oq5pl.mongodb.net/cluster?retryWrites=true&w=majority'
                 )
         # Item necesita encontrar la referencia la conexión
         # devuelta por connect()
