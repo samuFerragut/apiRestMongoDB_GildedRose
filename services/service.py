29e55a3c2f0f09981d0a12adccd43b0cbc9c4318
javiBarrientos <javibarrientosfernandez99@gmail.com>
2020-03-22 16:49:12 +0100

    feat(service): Añadir metodo inventario

--- services/service.py
+++ services/service.py
@@ -11,7 +11,16 @@ from repository.repo import Factory
 class Service():
 
     resource_fields = {
-            'name': fields.String,
-            'sell_in': fields.Integer,
-            'quality': fields.Integer
+        'name': fields.String,
+        'sell_in': fields.Integer,
+        'quality': fields.Integer
     }
+
+    @staticmethod
+    def Inventario():
+            db = get_db()
+
+            items = []
+            for item in g.Item.objects():
+                items.append(item)
+            return items
