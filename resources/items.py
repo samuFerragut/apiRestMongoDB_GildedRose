ef5dc632a3ff1678c7aa685b58cbcaa91ac2befb
samuFerragut <sferragut@cifpfbmoll.eu>
2020-03-23 11:46:25 +0100

    feat(resources.items): Crear get y post items

--- resources/items.py
+++ resources/items.py
@@ -1,3 +1,14 @@
 from flask_restful import Resource, Api, reqparse
 from repository.models import Item
 from services.service import Service
+
+
+class Items(Resource):
+
+    def get(self, itemName):
+        return Service.get_Item(item_name), 200
+
+    def post(self):
+        args = self.parseRequest()
+        Service.postItem(args)
+        return '', 201
