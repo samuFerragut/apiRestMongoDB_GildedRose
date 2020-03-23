8e1ad73937d0a5a83b3b24d85667bd87b48ec7e1
samuFerragut <sferragut@cifpfbmoll.eu>
2020-03-23 11:29:27 +0100

    feat(resources, inventario): Crear resource inventario

--- resources/inventario.py
+++ resources/inventario.py
@@ -2,3 +2,14 @@ from flask_restful import Resource
 # from repository.models import Item
 from services.service import Service
 
+
+class Inventario(Resource):
+
+    def get(self):
+        return Service.inventario(), 200
+
+    def post(self):
+        pass
+
+    def delete(self):
+        pass
