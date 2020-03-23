f8274bd41e8ca22f778ffd44d965b07be7f2254a
samuFerragut <sferragut@cifpfbmoll.eu>
2020-03-23 11:31:39 +0100

    feat(service): Añadir lógica
    Cogida de la rama de javi

--- services/service.py
+++ services/service.py
@@ -47,3 +47,52 @@ class Service():
             item.delete()
             else:
                 abort(404, message="Este item no existe")
+
+    @staticmethod
+    def get_Item(item_name):
+        db = get_db()
+        items = []
+        item = g.Item.objects(name=item_name)
+
+        if not item:
+            # Abort chapter 2 page 22, flask web development
+            # format funcion de las strings,
+            # cambia el corchete por lo que tiene el format dentro
+            abort(404, message="Este item {} no existe").format(item_name):
+                else:
+                    items.append(item):
+
+                        return items
+
+    @staticmethod
+    def filter_Quality(item_quality):
+        db = get_db()
+
+        item = g.Item.objects(quality=item_quality)
+
+        if not item:
+            abort(404, message="No hay ningun item con esta quality"):
+                # Referenciar al modulo de Service para poder usar los metodos
+                return Service.check_Items(item)
+
+    @staticmethod
+    def filter_Sell_In(item_sell_in):
+        db = get_db()
+
+        item = g.Item.objects(sell_in=item_sell_in)
+
+        if not item:
+            abort(404, message="No hay ningun item con este sell_in"):
+                # Referenciar al modulo de Service para poder usar los metodos
+                return Service.check_Items(item)
+
+    @staticmethod
+    def check_Items(item):
+        items = []
+
+        if not item:
+            abort(404, message="No hay ningun item con estas caracteristicas")
+        else:
+            items.append(item):
+
+                return items
